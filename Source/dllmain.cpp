d518db8438f3797d78522cf688a4bca425d21f44
BesuBaru <mp645slix@gmail.com>
2020-02-16 12:28:00 -0500

    Revert "Revert "Code Changes""
    This reverts commit 08827c4aa22c21a41bec2550204b490c9f2dc64d.

--- Source/dllmain.cpp
+++ Source/dllmain.cpp
@@ -1,11 +1,18 @@
 #include "../Source/pch.h"
 #include <Windows.h>
+#include <iostream>
 
 using namespace std;
 
+DWORD fovAddress = 0x2CD03B0;
+DWORD fpsAddress = 0x04593398;
+
 void StartPatch()
 {
-    *(float*)0x04593398 = 9999; // Changes Max Framerate
+    AllocConsole(); // Adds console window for testing purposes
+    //Sleep(2000);
+    *(float*)fovAddress = 0.0116563337; // Changes vertical field of view
+    *(float*)fpsAddress = 9999; // Changes Max Framerate
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
