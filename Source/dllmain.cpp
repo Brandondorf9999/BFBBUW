8ee908e9613927f171965ffa8173a5c9af3e9265
BesuBaru <mp645slix@gmail.com>
2020-02-16 21:17:35 -0500

    Implement INI parsing (Via IniPP) WIP
    Right now, it checks if the consolewindow value in config.ini is true or not.

--- Source/dllmain.cpp
+++ Source/dllmain.cpp
@@ -18,22 +18,56 @@
 #include "../Source/pch.h"
 #include <Windows.h>
 #include <iostream>
+#include <fstream>
+#include "ThirdParty/inipp/inipp/inipp.h"
 
 using namespace std;
 
 DWORD fovAddress = 0x2CD03B0;
 DWORD fpsAddress = 0x04593398;
+int tMaxFPS;
+int consoleWindow;
+bool debugBuild = false;
+
+void createConsole()
+{
+    AllocConsole(); // Adds console window for testing purposes
+    freopen_s((FILE**)stdout, "CONOUT$", "w", stdout); // Allows us to add outputs to the ASI Loader Console Window.
+    cout << "21xMachi9 Loaded!" << endl; // Tells us that the ASI Plugin loaded successfully.
+}
+
+void parseIni() //Parses settings from the config.ini file
+{
+    inipp::Ini<char> config; // Creates Inipp reference
+    std::ifstream is("config.ini"); // Checks for config.ini
+    config.parse(is); // if so, the config.ini will be parsed.
+    config.generate(std::cout);
+    config.default_section(config.sections["Settings"]);
+    config.generate(std::cout);
+    if (!debugBuild) // Checks if project build is Release, since creating another consoleWindow in Debug is redundant.
+    {
+        inipp::extract(config.sections["DebugSettings"]["ConsoleWindow"], consoleWindow);
+        if (consoleWindow = 1)
+        {
+            createConsole();
+        }
+    }
+    inipp::extract(config.sections["Settings"]["t.MaxFPS"], tMaxFPS); // Grabs t.MaxFPS from config.ini
+    cout << "tMaxFPS = " << tMaxFPS << endl; // Displays the ini's current framerate value
+}
 
 void StartPatch()
 {
     #if defined _DEBUG // Checks if the project build is Debug release.
-        AllocConsole(); // Adds console window for testing purposes
-        freopen_s((FILE**)stdout, "CONOUT$", "w", stdout); // Allows us to add outputs to the ASI Loader Console Window.
-        cout << "21xMachi9 Loaded!" << endl; // Tells us that the ASI Plugin loaded successfully.
-    #endif // Hides the debug window on Release builds.
+    {
+        debugBuild = true;
+        createConsole();
+    }
+    #endif // Hides the debug window on Release builds, if ConsoleWindow isn't open in config.ini.
+    parseIni();
     //Sleep(2000);
-    *(float*)fovAddress = 0.0116563337; // Changes vertical field of view.
-    *(float*)fpsAddress = 9999; // Changes Max Framerate.
+    //*(float*)fovAddress = 0.0116563337; // Changes vertical field of view.
+    //*(float*)fpsAddress = tMaxFPS; // Changes Max Framerate.
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
