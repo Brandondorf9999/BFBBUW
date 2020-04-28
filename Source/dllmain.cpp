9d44830e20cdb1c0374b5fe40beffd217e4c0d97
KingKrouch <mp645slix@outlook.com>
2020-04-28 15:23:40 -0400

    Making progress
    Read only memory causes the game to crash when memory values are modified, but that is being worked on.

--- Source/dllmain.cpp
+++ Source/dllmain.cpp
@@ -15,22 +15,38 @@
  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 **/
 
+#define _USE_MATH_DEFINES
+#define _CRT_SECURE_NO_WARNINGS
+
 #include "../Source/pch.h"
 #include "../Source/ThirdParty/inipp/inipp/inipp.h"
 #include <Windows.h>
 #include <iostream>
 #include <fstream>
 
-DWORD fovAddress = 0x2CD03B0;
-DWORD fpsAddress = 0x04593398;
 int tMaxFPS;
+float FOV;
 int consoleWindow;
 bool debugBuild = false;
+
+static void UnprotectModule(HMODULE p_Module)
+{
+    //This function was provided by Orfeasz
+    PIMAGE_DOS_HEADER s_Header = (PIMAGE_DOS_HEADER)p_Module;
+    PIMAGE_NT_HEADERS s_NTHeader = (PIMAGE_NT_HEADERS)((DWORD)p_Module + s_Header->e_lfanew);
+
+    SIZE_T s_ImageSize = s_NTHeader->OptionalHeader.SizeOfImage;
+
+    DWORD s_OldProtect;
+    VirtualProtect((LPVOID)p_Module, s_ImageSize, PAGE_EXECUTE_READWRITE, &s_OldProtect);
+}
 
 void createConsole()
 {
     AllocConsole(); // Adds console window for testing purposes
-    freopen_s((FILE**)stdout, "CONOUT$", "w", stdout); // Allows us to add outputs to the ASI Loader Console Window.
+    freopen("CONOUT$", "w", stdout); // Allows us to add outputs to the ASI Loader Console Window.
+    std::cout.clear();
+    std::cin.clear();
     std::cout << "21xMachi9 Loaded!" << std::endl; // Tells us that the ASI Plugin loaded successfully.
 }
 
@@ -45,34 +61,70 @@ void parseIni() //Parses settings from the config.ini file
     if (!debugBuild) // Checks if project build is Release, since creating another consoleWindow in Debug is redundant.
     {
         inipp::extract(config.sections["DebugSettings"]["ConsoleWindow"], consoleWindow);
-        if (consoleWindow = 1)
+        if (consoleWindow == 1)
         {
             createConsole();
         }
     }
     inipp::extract(config.sections["Settings"]["t.MaxFPS"], tMaxFPS); // Grabs t.MaxFPS from config.ini
-    std::cout << "tMaxFPS = " << tMaxFPS << std::endl; // Displays the ini's current framerate value
+
+    inipp::Ini<char> GUSIni;
+    char* localappdata = getenv("LOCALAPPDATA");
+
+}
+
+void FOVCalc()
+{
+    // Declare the Vertical and Horizoontal Resolution variables
+    int hRes = 0;
+    int vRes = 0;
+
+    // Declares the original 16:9 Vert- FOV
+    float originalFOV = 0.008726646192;
+    float originalAspectRatio = 1.777777777777778;
+
+    // If GameUserSettings.ini exists, grab in-game resolution from that
+    {
+
+    }
+    // Otherwise, get desktop resolution
+    {
+        
+    }
+    // Convert the int values to floats, so then we can determine the aspect ratio
+    float AspectRatio = (float)hRes / (float)vRes;
+
+    //Arctan(Tan(originalFOV * (float)M_PI / 360.0f) / (AspectRatio) * (originalAspectRatio)) * 360.0f / (float)M_PI
 }
 
 void StartPatch()
 {
-    #if defined _DEBUG // Checks if the project build is Debug release.
+#if defined _DEBUG // Checks if the project build is Debug release.
     {
         debugBuild = true;
         createConsole();
     }
-    #endif // Hides the debug window on Release builds, if ConsoleWindow isn't open in config.ini.
+#endif // Hides the debug window on Release builds, if ConsoleWindow isn't open in config.ini.
+
     parseIni();
-    //Sleep(2000);
-    //*(float*)fovAddress = 0.0116563337; // Changes vertical field of view.
-    //*(float*)fpsAddress = tMaxFPS; // Changes Max Framerate.
+    // Gets the base module address, and also unprotects it from write protection.
+    HMODULE baseModule = GetModuleHandle(NULL);
+    UnprotectModule(baseModule);
+
+    Sleep(10000); // Sleeps the thread for ten seconds before applying the memory values.
+    //Writes FPS Cap to Memory
+    *(float*)(*((intptr_t*)((intptr_t)baseModule + 0x4593398)) + 0x0) = tMaxFPS;
+    // Writes FOV to Memory
+    *(float*)((intptr_t)baseModule + 0x2CD03B0) = 0.0116563337; // Test change to 21:9 Vert- FOV, will implement proper FOV scaling later
+    // Writes Pillarbox Removal into Memory ("33 83 4C 02" to "33 83 4C 00")
+    *(BYTE*)((intptr_t)baseModule + 0x1E14850 + 0x3) = 00;
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
     if (ul_reason_for_call == DLL_PROCESS_ATTACH) // This code runs when the DLL file has been attached to the game process.
     {
-        StartPatch(); // Calls the StartPatch function on start.
+        CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)StartPatch, NULL, NULL, NULL); // Calls the StartPatch function in a new thread on start.
     }
     return TRUE;
 }
