08e9d6c895b8b2cb270e0a422e1bc0b583fd8387
KingKrouch <mp645slix@outlook.com>
2020-04-28 20:55:45 -0400

    Remove unused code

--- Source/dllmain.cpp
+++ Source/dllmain.cpp
@@ -64,21 +64,6 @@ void parseIni() //Parses settings from the config.ini file.
     inipp::extract(config.sections["Settings"]["t.MaxFPS"], tMaxFPS); // Grabs "t.MaxFPS" from config.ini.
 }
 
-// Get the horizontal and vertical screen sizes in pixel
-void GetDesktopResolution(int& horizontal, int& vertical)
-{
-    RECT desktop;
-    // Get a handle to the desktop window
-    const HWND hDesktop = GetDesktopWindow();
-    // Get the size of screen to the variable desktop
-    GetWindowRect(hDesktop, &desktop);
-    // The top left corner will have coordinates (0,0)
-    // and the bottom right corner will have coordinates
-    // (horizontal, vertical)
-    horizontal = desktop.right;
-    vertical = desktop.bottom;
-}
-
 void FOVCalc()
 {
 
